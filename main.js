58254b05877c395379fcaccf42f89856a6c88a73
Angel <angelSistemas21@gmail.com>
2022-09-29 16:25:40 -0500

    feat: Se agregó la función btnEncriptar

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const textWrite  = document.querySelector('.text-write');
 const textResult = document.querySelector('.text-result');
 const buttonCopy = document.querySelector('.btn-copy');
 
+const btnEncriptar = () =>{
+    const encryptedText = encriptar(textWrite.value);
+    textResult.value = encryptedText;
+    textResult.style.backgroundImage = 'none';
+    buttonCopy.style.display = 'block';
+    buttonCopy.style.marginBottom = '15px';
+}
+
 const encriptar = (encryptedText) =>{
     let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
     encryptedText  = encryptedText.toLowerCase();
