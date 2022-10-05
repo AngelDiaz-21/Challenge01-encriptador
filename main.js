2855e4f3187962d4c0d9a1134aea20b55acc7d4d
Angel <angelSistemas21@gmail.com>
2022-10-05 12:23:00 -0500

    feat: Función verificar, que permite hacer una validación para activar los botones

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const textWrite  = document.querySelector('.text-write');
 const textResult = document.querySelector('.text-result');
 const buttonCopy = document.querySelector('.btn-copy');
+const buttonEncriptar = document.getElementById("btn-encriptar");
+const buttonDesencriptar = document.getElementById("btn-desencriptar");
+const warningBtn = document.querySelector('.warning-button');
 
 const btnEncriptar = () =>{
     const encryptedText = encriptar(textWrite.value);
@@ -60,3 +63,19 @@ const getScreenSizeToChangeImage = () => {
     return (screen.width < 1200) ? textResult.style.backgroundImage = "url('assets/mensaje-noEncontrado720.svg')" 
                                     : textResult.style.backgroundImage = "url('assets/buscando-personaje.svg')"
 };
+
+function verificar(valor) {
+    if (valor.length >= 3) {
+        buttonEncriptar.disabled = false;
+        buttonDesencriptar.disabled = false;
+        buttonEncriptar.classList.remove("disabled");
+        buttonDesencriptar.classList.remove("disabled");
+        warningBtn.style.display = 'none';
+    } else {
+        buttonEncriptar.disabled = true;
+        buttonDesencriptar.disabled = true;
+        buttonEncriptar.classList.add("disabled");
+        buttonDesencriptar.classList.add("disabled");
+        warningBtn.style.display = 'block';
+    }
+}
